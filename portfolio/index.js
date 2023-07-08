afa28ea63868949b121e1321c70fba76feee0063
Esiri1960 <oghoishanbee1960@gmail.com>
2023-07-08 15:59:49 +0100

    Portfolio files upload

---
+++ portfolio/index.js
@@ -0,0 +1,74 @@
+
+// let heading = document.querySelector(".h1");
+// heading.innerHTML = "go!"
+let menuItem = document.querySelectorAll(".menu__item-text");
+let menuImage = document.querySelectorAll(".menu__item-image");
+
+// adding eventListeners to all the menuItems.
+for (let i = 0; i < 4; i++) {
+  //   image reveal animation
+  const animation = gsap.to(menuImage[i], {
+    opacity: 1,
+    duration: 0.2,
+    scale: 1,
+    ease: "ease-in-out"
+  });
+
+  menuItem[i].addEventListener("mouseenter", () => animation.play());
+  menuItem[i].addEventListener("mouseleave", () => animation.reverse());
+
+  //   initialization
+  animation.reverse();
+}
+
+//   to move image along with cursor
+function moveText(e) {
+  gsap.to([...menuImage], {
+    css: {
+      left: e.pageX + 50,
+      top: e.pageY,
+    },
+    duration: 0.3,
+  });
+}
+
+menuItem.forEach((el) => {
+  el.addEventListener("mousemove", moveText);
+});
+
+
+
+
+
+
+// // Scroll to specific values
+// // scrollTo is the same
+// window.scroll({
+//   top: 2500,
+//   left: 0,
+//   behavior: 'smooth'
+// });
+//
+// // Scroll certain amounts from current position
+// window.scrollBy({
+//   top: 100, // could be negative value
+//   left: 0,
+//   behavior: 'smooth'
+// });
+
+
+
+
+// SMOOTH SCROLLING working version
+const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+});
+
+function raf(time) {
+    lenis.raf(time);
+    ScrollTrigger.update();
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
